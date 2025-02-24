5d63e315f94af5860fe38f85eea8e1f7199c10b7
KevinKhuu01 <kevinkhuu01@gmail.com>
2025-02-23 21:58:55 -0500

    updated base frame

---
+++ shadersweb/src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+      <h1>Welcome to Shaders Web!</h1>
+  );
+}
+
+export default App;
---
+++ shadersweb/src/components/Canvas.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CanvasWidget(){
+    return <div></div>
+}
+
+export default CanvasWidget;
