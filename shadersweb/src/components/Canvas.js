3b035ca26ec2fb994b21b72ade54c4c8b9a108a9
CARLOS\carlo <carlosmatecki2@gmail.com>
2025-02-24 20:56:57 -0500

    Is working but I dont like this solution

--- shadersweb/src/components/Canvas.js
+++ shadersweb/src/components/Canvas.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 import {CreateWebGPUCanvas} from "../Engine/Core";
+import  {useEffect, useState} from "react";
 
-function CanvasWidget(){
-    return <div></div>
+function CanvasWidget() {
+    const canvasElement = useState(null);
+
+    useEffect(() => {
+        CreateWebGPUCanvas(800, 800).then(canvas => {
+            if(canvasElement.current){
+                canvasElement.current.appendChild(canvas);
+            }
+        });
+    }, []);
+
+    return <div ref={canvasElement}></div>; // This will now show the actual canvas
 }
 
 export default CanvasWidget;
