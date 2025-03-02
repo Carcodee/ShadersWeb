d6518eb122e3a630a63e75de2d7185a45cea78f0
CARLOS\carlo <carlosmatecki2@gmail.com>
2025-03-02 15:34:47 -0500

    Adding engine builder classes to make the pipeline building process more modular

---
+++ shadersweb/src/Engine/BindGroupBuilder.js
@@ -0,0 +1,34 @@
+﻿
+class BindGroupBuilder{
+    bindGroupLayoutEntries = []
+    bindGroupEntries = []
+    bindGroupLayout;
+    bindGroup;
+
+    constructor() {
+    }
+    AddBuffer(binding, visibility, bufferType, buffer){
+        this.bindGroupLayoutEntries.push({binding: binding, visibility: visibility, buffer:  bufferType})
+        this.bindGroupEntries.push({binding: binding, resource: {buffer: buffer}})
+    }
+    AddImage(binding, visibility, textureView){
+        this.bindGroupLayoutEntries.push({binding, visibility, texture : {}})
+        this.bindGroupEntries.push({binding: binding, resource: textureView})
+    }
+    AddSampler(binding, visibility, sampler){
+        this.bindGroupLayoutEntries.push({binding, visibility, sampler : {}})
+        this.bindGroupEntries.push({binding: binding, resource: sampler})
+    }
+    Build(device){
+
+        this.bindGroupLayout = device.createBindGroupLayout({
+            entries: this.bindGroupLayoutEntries,
+        })
+
+        this.bindGroup = device.createBindGroup({
+            layout: this.bindGroupLayout,
+            entries: this.bindGroupEntries,
+        })
+    }
+
+}
--- shadersweb/src/Engine/PipelineBuilder.js
+++ shadersweb/src/Engine/PipelineBuilder.js
@@ -3,12 +3,20 @@ class PipelineBuilder{
 
     device;
     canvasFormat;
+    vertexBufferLayout;
+    shaderModule;
 
     PipelineBuilder(device){
         this.device = device;
     }
     SetCanvasFormat(canvasFormat){
         this.canvasFormat = canvasFormat;
+    }
+    SetVertexBufferLayout(vertexBufferLayout){
+        this.vertexBufferLayout = vertexBufferLayout;
+    }
+    SetShaderModule(shaderModule){
+        this.shaderModuele = shaderModule;
     }
 
 }
